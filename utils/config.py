c201aa4c7c7832d194fe4b0c2c54dc27b35ea0b4
Alice Varella <alice.varella@gmail.com>
2026-06-30 22:25:46 -0300

    initial constants

---
+++ utils/config.py
@@ -0,0 +1,8 @@
+"""
+constants and parameters
+"""
+
+# absolute path for outputs, experiments and so on
+from pathlib import Path
+ROOT_PATH = Path(__file__).parent.parent
+DATA_PATH = ROOT_PATH / "data"
