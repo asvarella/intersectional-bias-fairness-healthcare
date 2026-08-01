7303eab9e22c43460829b028ab9cfcd1f1398a3a
Alice Varella <alice.varella@gmail.com>
2026-08-01 20:55:07 -0300

    feat: AIDS dataset with global config setup

--- utils/config.py
+++ utils/config.py
@@ -6,3 +6,22 @@ constants and parameters
 from pathlib import Path
 ROOT_PATH = Path(__file__).parent.parent
 DATA_PATH = ROOT_PATH / "data"
+
+
+
+'''
+Dataset constants and configuration
+'''
+
+# 1. AIDS
+DATASETS = {
+    "AIDS" : {
+        "dataset_name": "AIDS",
+        "sensitive_attrs": ["race", "gender", "homo"],
+        "secondary_attrs": ["age_group"],
+        "target_col": "target",
+        "favorable_label": 0,
+        "processed_csv_file": "6_aids_pp.csv",
+        "min_sub_count": 60
+    },
+}
